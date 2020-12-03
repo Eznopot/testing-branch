cfd6c283bbde181631811c1b5726e91de2ccc458
Remy Salem <remy12.salem@epitech.eu>
2020-12-03 12:40:04 +0100

    aff a new serv wtf

---
+++ server/main.c
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2020
+** testing-branch
+** File description:
+** main
+*/
+
+void serv() {
+    printf("je suis la partie serveur\n");
+}
+
+int main() {
+    serv();
+    return (0);
+}
